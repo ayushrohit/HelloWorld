be7943e31fd81b601ade3aad13cabfebefaeaa05
Ayush <you@example.com>
2021-03-30 01:09:57 +0530

    calling from main class

--- src/simple/SumOfNaturalNum.java
+++ src/simple/SumOfNaturalNum.java
@@ -3,35 +3,23 @@ package simple;
 import java.util.Scanner;
 
 public class SumOfNaturalNum {
-	
-	int sumOfNaturalNum(int n){
-		return n*(n+1)/2;
-		
-	}
-	
-	public static void main(String[] args)
-	{
+	;
+	void  sumOfNaturalNum(){
 		int sum=0;
 		int i=0;
-		
-		SumOfNaturalNum a = new SumOfNaturalNum();
+		 
 		Scanner sc = new Scanner(System.in);
-		try{
 		System.out.println("Enter the value till which you wants to find the sum.");
 		i=sc.nextInt();
 		
-		sum= a.sumOfNaturalNum(i);
+		sum=  i*(i+1)/2;
 		
 		System.out.println("The sum of first " +  i  +  " natural number is :" + sum);
+		
 		}
-		catch(Exception e){
-			System.out.println("Out of limit");
-			//System.out.println(e);
+		
 		}
 		
 			
-		   }
-	}
+		   
 	
-
-
